b51991219d6a46bd851a25e66eba498cb3beb323
Neha Malcom Francis <n-francis@ti.com>
2024-06-05 15:33:07 +0530

    board: ti: j722s: evm.c: Fixup DDR size when ECC is enabled
    Call k3-ddrss driver through fixup_ddr_driver_for_ecc() to fixup the
    device tree and resize the available amount of DDR, if ECC is enabled in
    R5. If not enabled, propagate fixups (if any) by calling
    fixup_memory_node().
    
    Signed-off-by: Neha Malcom Francis <n-francis@ti.com>

--- board/ti/j722s/evm.c
+++ board/ti/j722s/evm.c
@@ -19,3 +19,15 @@ int board_init(void)
 {
 	return 0;
 }
+
+#if defined(CONFIG_SPL_BUILD)
+void spl_perform_fixups(struct spl_image_info *spl_image)
+{
+	if (IS_ENABLED(CONFIG_K3_DDRSS)) {
+		if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+			fixup_ddr_driver_for_ecc(spl_image);
+	} else {
+		fixup_memory_node(spl_image);
+	}
+}
+#endif
