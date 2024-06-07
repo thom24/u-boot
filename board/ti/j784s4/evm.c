3d101d595800fae13affa082d13108f8a2f5abee
Apurva Nandan <a-nandan@ti.com>
2024-06-07 13:31:30 +0530

    board: ti: j784s4: Enable OSPI NAND SPL fixup
    On J784S4, SW3.1 (WKUP_GPIO0_6) is used for selecting between OSPI NOR and
    OSPI NAND flash. Read the GPIO state, to perform flash enable operation on
    the fdt, for the valid flash and delete the unconnected flash node.
    
    Signed-off-by: Apurva Nandan <a-nandan@ti.com>
    Signed-off-by: Prasanth Babu Mantena <p-mantena@ti.com>

--- board/ti/j784s4/evm.c
+++ board/ti/j784s4/evm.c
@@ -7,16 +7,61 @@
  *
  */
 
+#include "../common/fdt_ops.h"
+#include <env.h>
+#include <fdt_support.h>
+#include <generic-phy.h>
+#include <image.h>
 #include <init.h>
+#include <log.h>
+#include <net.h>
+#include <asm/arch/hardware.h>
+#include <asm/gpio.h>
+#include <asm/io.h>
 #include <spl.h>
 #include "../common/fdt_ops.h"
 #include "../common/k3-ddr-init.h"
+#include <dm.h>
+#include <dm/uclass-internal.h>
+
+#include "../common/board_detect.h"
 
 DECLARE_GLOBAL_DATA_PTR;
 
 int board_init(void)
 {
 	return 0;
+}
+
+/* Enables the spi-nand dts node, if onboard mux is set to spinand */
+static void __maybe_unused detect_enable_spinand(void *blob)
+{
+	if (IS_ENABLED(CONFIG_DM_GPIO) && IS_ENABLED(CONFIG_OF_LIBFDT)) {
+		struct gpio_desc desc = {0};
+		char *ospi_mux_sel_gpio = "6";
+		int nand_offset, nor_offset;
+
+		if (dm_gpio_lookup_name(ospi_mux_sel_gpio, &desc))
+			return;
+
+		if (dm_gpio_request(&desc, ospi_mux_sel_gpio))
+			return;
+
+		if (dm_gpio_set_dir_flags(&desc, GPIOD_IS_IN))
+			return;
+
+		nand_offset = fdt_node_offset_by_compatible(blob, -1, "spi-nand");
+		nor_offset = fdt_node_offset_by_compatible(blob,
+							   fdt_parent_offset(blob, nand_offset),
+							   "jedec,spi-nor");
+
+		if (dm_gpio_get_value(&desc)) {
+			fdt_status_okay(blob, nand_offset);
+			fdt_del_node(blob, nor_offset);
+		} else {
+			fdt_del_node(blob, nand_offset);
+		}
+	}
 }
 
 #if defined(CONFIG_SPL_BUILD)
@@ -28,6 +73,9 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 	} else {
 		fixup_memory_node(spl_image);
 	}
+	detect_enable_spinand(spl_image->fdt_addr);
+	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
+		fixup_ddr_driver_for_ecc(spl_image);
 }
 #endif
 
@@ -42,3 +90,13 @@ int board_late_init(void)
 void spl_board_init(void)
 {
 }
+
+#if defined(CONFIG_OF_LIBFDT) && defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	detect_enable_spinand(blob);
+
+	return 0;
+}
+#endif
+
