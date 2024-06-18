1325a7e6a58752f3567aebc70ffc763b9660cbad
Vaishnav Achath <vaishnav.a@ti.com>
2024-06-18 16:19:05 +0530

    board: ti: j722s: Add fixups for SPI NOR and NAND
    SPI NOR and NAND flash is muxed in J722S EVM, add necessary
    fixups to support the same.
    
    Signed-off-by: Vaishnav Achath <vaishnav.a@ti.com>
    Signed-off-by: Prasanth Babu Mantena <p-mantena@ti.com>

--- board/ti/j722s/evm.c
+++ board/ti/j722s/evm.c
@@ -7,13 +7,67 @@
  */
 
 #include <asm/arch/hardware.h>
+#include <asm/gpio.h>
 #include <asm/io.h>
-#include <dm/uclass.h>
+#include <dm.h>
+#include <dm/uclass-internal.h>
+#include <dm/root.h>
 #include <env.h>
 #include <fdt_support.h>
 #include <spl.h>
 
 #include "../common/k3-ddr-init.h"
+
+ofnode cadence_qspi_get_subnode(struct udevice *dev)
+{
+	if (IS_ENABLED(CONFIG_SPL_BUILD) &&
+	    IS_ENABLED(CONFIG_TARGET_J721S2_R5_EVM)) {
+		if (spl_boot_device() == BOOT_DEVICE_SPINAND)
+			return ofnode_by_compatible(dev_ofnode(dev), "spi-nand");
+	}
+
+	return dev_read_first_subnode(dev);
+}
+
+/* Enables the spi-nand dts node, if onboard mux is set to spinand */
+static void __maybe_unused detect_enable_spinand(void *blob)
+{
+	if (IS_ENABLED(CONFIG_DM_GPIO) && IS_ENABLED(CONFIG_OF_LIBFDT)) {
+		struct gpio_desc desc = {0};
+		char *ospi_mux_sel_gpio = "1";
+		int nand_offset, nor_offset;
+
+		if (dm_gpio_lookup_name(ospi_mux_sel_gpio, &desc))
+			return;
+
+		if (dm_gpio_request(&desc, ospi_mux_sel_gpio))
+			return;
+
+		if (dm_gpio_set_dir_flags(&desc, GPIOD_IS_IN))
+			return;
+
+		nand_offset = fdt_node_offset_by_compatible(blob, -1, "spi-nand");
+		nor_offset = fdt_node_offset_by_compatible(blob,
+							   fdt_parent_offset(blob, nand_offset),
+							   "jedec,spi-nor");
+
+		if (dm_gpio_get_value(&desc)) {
+			fdt_status_okay(blob, nand_offset);
+			fdt_del_node(blob, nor_offset);
+		} else {
+			fdt_del_node(blob, nand_offset);
+		}
+	}
+}
+
+#if defined(CONFIG_OF_LIBFDT) && defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	detect_enable_spinand(blob);
+
+	return 0;
+}
+#endif
 
 int board_init(void)
 {
@@ -29,5 +83,7 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 	} else {
 		fixup_memory_node(spl_image);
 	}
+
+	detect_enable_spinand(spl_image->fdt_addr);
 }
 #endif
