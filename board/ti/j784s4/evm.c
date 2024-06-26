5c0e7bd633f8e1ecd1d3302fa7fd456a395ac5d7
Neha Malcom Francis <n-francis@ti.com>
2024-06-26 13:17:59 +0530

    board: ti: j784s4: evm.c: Remove duplicate fixup
    Remove the additional fixup_ddr_driver_for_ecc() which results in
    DT memory node fixup happening twice.
    
    Fixes: 3d101d595800 ("board: ti: j784s4: Enable OSPI NAND SPL fixup")
    Signed-off-by: Neha Malcom Francis <n-francis@ti.com>

--- board/ti/j784s4/evm.c
+++ board/ti/j784s4/evm.c
@@ -74,8 +74,6 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 		fixup_memory_node(spl_image);
 	}
 	detect_enable_spinand(spl_image->fdt_addr);
-	if (IS_ENABLED(CONFIG_K3_INLINE_ECC))
-		fixup_ddr_driver_for_ecc(spl_image);
 }
 #endif
 
