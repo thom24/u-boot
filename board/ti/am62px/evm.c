b643de4a3426d414323c4ea0866e59f3bf25cc39
Devarsh Thakkar <devarsht@ti.com>
2024-06-27 21:13:41 +0530

    board: ti: am62p: Add splash screen support
    - Add MMC and OSPI NOR flash as storage locations for splash screen.
    - Enable video memory reservation and splash display by calling board
      specific routine for splash screen.
    - Enable simple-framebuffer node and add memory reservation in Kernel DT
      to enable flicker-free transition while booting Kernel.
    
    Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

--- board/ti/am62px/evm.c
+++ board/ti/am62px/evm.c
@@ -12,10 +12,38 @@
 #include <dm/uclass.h>
 #include <env.h>
 #include <fdt_support.h>
+#include <fdt_simplefb.h>
 #include <spl.h>
+#include <splash.h>
+#include <video.h>
 
 #include "../common/fdt_ops.h"
 #include "../common/k3-ddr-init.h"
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#if CONFIG_IS_ENABLED(SPLASH_SCREEN)
+static struct splash_location default_splash_locations[] = {
+	{
+		.name = "sf",
+		.storage = SPLASH_STORAGE_SF,
+		.flags = SPLASH_STORAGE_RAW,
+		.offset = 0x700000,
+	},
+	{
+		.name		= "mmc",
+		.storage	= SPLASH_STORAGE_MMC,
+		.flags		= SPLASH_STORAGE_FS,
+		.devpart	= "1:1",
+	},
+};
+
+int splash_screen_prepare(void)
+{
+	return splash_source_load(default_splash_locations,
+				ARRAY_SIZE(default_splash_locations));
+}
+#endif
 
 int board_init(void)
 {
@@ -44,6 +72,28 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 void spl_board_init(void)
 {
 	enable_caches();
+	if (IS_ENABLED(CONFIG_SPL_SPLASH_SCREEN) && IS_ENABLED(CONFIG_SPL_BMP))
+		splash_display();
 }
+#endif
 
+#if defined(CONFIG_OF_BOARD_SETUP)
+int ft_board_setup(void *blob, struct bd_info *bd)
+{
+	int ret = -1;
+
+	if (IS_ENABLED(CONFIG_FDT_SIMPLEFB))
+		ret = fdt_simplefb_enable_and_mem_rsv(blob);
+
+	/* If simplefb is not enabled and video is active, then at least reserve
+	 * the framebuffer region to preserve the splash screen while OS is booting
+	 */
+
+	if (IS_ENABLED(CONFIG_VIDEO)) {
+		if (ret && video_is_active())
+			return fdt_add_fb_mem_rsv(blob);
+	}
+
+	return 0;
+}
 #endif
