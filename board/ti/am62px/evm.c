12df22ea7937de32b044be0b42b81da16d29df06
Vishal Mahaveer <vishalm@ti.com>
2024-07-11 12:00:49 +0530

    board: ti: am62px: Add basic initialization for 32k crystal
    Do the basic configuration required for enabling the 32k crystal. If
    external 32k source is not used, 32k rc-osc comes into play, which is
    accurate to +-20%.
    
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- board/ti/am62px/evm.c
+++ board/ti/am62px/evm.c
@@ -71,6 +71,17 @@ void spl_perform_fixups(struct spl_image_info *spl_image)
 
 void spl_board_init(void)
 {
+	u32 val;
+
+	/* We have 32k crystal, so lets enable it */
+	val = readl(MCU_CTRL_LFXOSC_CTRL);
+	val &= ~(MCU_CTRL_LFXOSC_32K_DISABLE_VAL);
+	writel(val, MCU_CTRL_LFXOSC_CTRL);
+	/* Add any TRIM needed for the crystal here.. */
+	/* Make sure to mux up to take the SoC 32k from the crystal */
+	writel(MCU_CTRL_DEVICE_CLKOUT_LFOSC_SELECT_VAL,
+	       MCU_CTRL_DEVICE_CLKOUT_32K_CTRL);
+
 	enable_caches();
 	if (IS_ENABLED(CONFIG_SPL_SPLASH_SCREEN) && IS_ENABLED(CONFIG_SPL_BMP))
 		splash_display();
