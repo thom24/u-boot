818c76aed67f94059b9ebb94d237418802620b81
Beleswar Padhi <b-padhi@ti.com>
2024-08-01 17:46:01 +0530

    HACK: arch: mach-k3: Mark memory region as non-cached for J722S
    Memory region used by remote cores was set to non-cached region
    but commit 7c9c6e192580 ("arm: mach-k3: Merge initial memory maps")
    makes all memory region unified across K3 devices as cached.
    
    As normal course of operation, remote proc driver does data cache flush
    after copying firmware from elf to remote core's region.
    Even after flush, R5 rproc was showing some inconsistency while booting
    remote cores.
    Therefore, as a HACK, Mark R5 firmware region as Non-Cached.
    
    Cc: Hari Nagalla <hnagalla@ti.com>
    Cc: Andrew Davis <afd@ti.com>
    Cc: Udit Kumar <u-kumar1@ti.com>
    Signed-off-by: Beleswar Padhi <b-padhi@ti.com>

--- arch/arm/mach-k3/arm64-mmu.c
+++ arch/arm/mach-k3/arm64-mmu.c
@@ -265,8 +265,58 @@ struct mm_region soc_mem_map[NR_MMU_REGIONS] = {
 
 #endif /* CONFIG_SOC_K3_J784S4 */
 
+#if defined(CONFIG_SOC_K3_J722S)
+#define NR_MMU_REGIONS	(CONFIG_NR_DRAM_BANKS + 5)
+
+struct mm_region soc_mem_map[NR_MMU_REGIONS] = {
+	{
+		.virt = 0x0UL,
+		.phys = 0x0UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		.virt = 0x80000000UL,
+		.phys = 0x80000000UL,
+		.size = 0x1e780000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0xa0000000UL,
+		.phys = 0xa0000000UL,
+		.size = 0x3000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL_NC) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0xa3000000UL,
+		.phys = 0xa3000000UL,
+		.size = 0x5d000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x880000000UL,
+		.phys = 0x880000000UL,
+		.size = 0x80000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_NORMAL) |
+			 PTE_BLOCK_INNER_SHARE
+	}, {
+		.virt = 0x500000000UL,
+		.phys = 0x500000000UL,
+		.size = 0x380000000UL,
+		.attrs = PTE_BLOCK_MEMTYPE(MT_DEVICE_NGNRNE) |
+			 PTE_BLOCK_NON_SHARE |
+			 PTE_BLOCK_PXN | PTE_BLOCK_UXN
+	}, {
+		/* List terminator */
+		0,
+	}
+};
+
+#endif /* CONFIG_SOC_K3_J722S */
+
 #if defined(CONFIG_SOC_K3_J721E) || defined(CONFIG_SOC_K3_J721S2) ||\
-	 defined(CONFIG_SOC_K3_J784S4)
+	 defined(CONFIG_SOC_K3_J784S4) || defined(CONFIG_SOC_K3_J722S)
 struct mm_region *mem_map = soc_mem_map;
 #else
 struct mm_region *mem_map = k3_mem_map;
