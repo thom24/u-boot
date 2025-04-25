efe84eadfd6de0050c11294ec6a0282d8de5ef26
Thomas Richard <thomas.richard@bootlin.com>
2025-04-25 11:26:25 +0200

    resume is working on HS using rproc_load() and rproc_start()
    Signed-off-by: Thomas Richard <thomas.richard@bootlin.com>

--- arch/arm/mach-k3/common.c
+++ arch/arm/mach-k3/common.c
@@ -382,60 +382,75 @@ void __noreturn jump_to_image_no_args(struct spl_image_info *spl_image)
 		debug("restore TFA in MSMC done\n");
 
 
-		struct power_domain rproc_pwrdmn;
-		unsigned long gtc_rate;
-		struct udevice *dev;
-		struct clk gtc_clk;
-		void *gtc_base;
+//		struct power_domain rproc_pwrdmn;
+//		unsigned long gtc_rate;
+//		struct udevice *dev;
+//		struct clk gtc_clk;
+//		void *gtc_base;
+//
+//		debug ("Start to power on power domain of A72\n");
+//
+//		ret = uclass_get_device_by_seq(UCLASS_REMOTEPROC, 1, &dev);
+//		if (ret)
+//			panic("Unknown remote processor 1 (%d)\n", ret);
+//
+//		ret = power_domain_get_by_index(dev, &rproc_pwrdmn, 1);
+//		if (ret)
+//			panic("power_domain_get_rproc() failed: %d\n", ret);
+//
+//		ret = clk_get_by_index(dev, 0, &gtc_clk);
+//		if (ret)
+//			panic("clk_get failed: %d\n", ret);
+//
+//		gtc_base = dev_read_addr_ptr(dev);
+//		if (!gtc_base)
+//			panic("Get GTC address failed\n");
+//
+//		gtc_rate = clk_get_rate(&gtc_clk);
+//
+//#define GTC_CNTCR_REG	0x0
+//#define GTC_CNTFID0_REG	0x20
+//#define GTC_CNTR_EN	0x3
+//		/* TFA expect the Global Timebase Counter to be set-up */
+//		writel((u32)gtc_rate, gtc_base + GTC_CNTFID0_REG);
+//		writel(GTC_CNTR_EN, gtc_base + GTC_CNTCR_REG);
+//
+//		ret = power_domain_on(&rproc_pwrdmn);
+//		if (ret)
+//			panic("power_domain_on failed: %d\n", ret);
+//
+//		struct k3_arm64_privdata *rproc = dev_get_priv(dev);
+//		if (rproc->has_cluster_node) {
+//			debug("Start power domain of cluster\n");
+//			ret = power_domain_on(&rproc->cluster_pwrdmn);
+//			if (ret) {
+//				panic("power_domain_on(&rproc->cluster_pwrdmn) failed: %d\n",
+//					ret);
+//			}
+//		}
+//
+//		/* restore TFA resume vectore address in main core */
+//		debug("restore TFA resume vector\n");
+//		ret = ti_sci->ops.lpm_ops.core_resume(ti_sci);
+//		if (ret)
+//			panic("ATF failed to resume (%d)\n", ret);
+//
+		debug("restore TFA resume vector done skipped\n");
 
-		debug ("Start to power on power domain of A72\n");
 
-		ret = uclass_get_device_by_seq(UCLASS_REMOTEPROC, 1, &dev);
+		debug("rproc_load(1, 0x70000128, 0x200)\n");
+		ret = rproc_load(1, 0x70000128, 0x200);
 		if (ret)
-			panic("Unknown remote processor 1 (%d)\n", ret);
+			panic("%s: ATF failed to load on rproc (%d)\n", __func__, ret);
+		debug("rproc_load(1, 0x70000128, 0x200) done\n");
 
-		ret = power_domain_get_by_index(dev, &rproc_pwrdmn, 1);
+		debug("rproc_start(1)\n");
+		ret = rproc_start(1);
 		if (ret)
-			panic("power_domain_get_rproc() failed: %d\n", ret);
+			panic("%s: ATF failed to start on rproc (%d)\n",
+			      __func__, ret);
+		debug("rproc_start(1) done\n");
 
-		ret = clk_get_by_index(dev, 0, &gtc_clk);
-		if (ret)
-			panic("clk_get failed: %d\n", ret);
-
-		gtc_base = dev_read_addr_ptr(dev);
-		if (!gtc_base)
-			panic("Get GTC address failed\n");
-
-		gtc_rate = clk_get_rate(&gtc_clk);
-
-#define GTC_CNTCR_REG	0x0
-#define GTC_CNTFID0_REG	0x20
-#define GTC_CNTR_EN	0x3
-		/* TFA expect the Global Timebase Counter to be set-up */
-		writel((u32)gtc_rate, gtc_base + GTC_CNTFID0_REG);
-		writel(GTC_CNTR_EN, gtc_base + GTC_CNTCR_REG);
-
-		ret = power_domain_on(&rproc_pwrdmn);
-		if (ret)
-			panic("power_domain_on failed: %d\n", ret);
-
-		struct k3_arm64_privdata *rproc = dev_get_priv(dev);
-		if (rproc->has_cluster_node) {
-			debug("Start power domain of cluster\n");
-			ret = power_domain_on(&rproc->cluster_pwrdmn);
-			if (ret) {
-				panic("power_domain_on(&rproc->cluster_pwrdmn) failed: %d\n",
-					ret);
-			}
-		}
-
-		/* restore TFA resume vectore address in main core */
-		debug("restore TFA resume vector\n");
-		ret = ti_sci->ops.lpm_ops.core_resume(ti_sci);
-		if (ret)
-			panic("ATF failed to resume (%d)\n", ret);
-
-		debug("restore TFA resume vector done\n");
 		goto start_arm64;
 	}
 #endif /* IS_ENABLED(CONFIG_SOC_K3_J721E) */
