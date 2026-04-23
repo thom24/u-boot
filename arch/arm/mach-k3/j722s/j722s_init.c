MontaVista-OpenSourceTechnology/ti-u-boot

commit f68a776b69d3968f9a39bbb881910bbde2efa31b

FROMLIST: mach-k3: Defer CTRL_MMR access until after TIFS version response
The CTRL_MMR registers are firewalled at boot time, and TIFS unlock
them as part of the system firmware initialization sequence.

Current code assumes the CTRL_MMR space can be accessible at early
stage of board_init_f execution by unlocking them.

But on HS-SE devices, TIFS takes more time to complete initialization,
and CTRL_MMR registers remains firewalled and not getting unlocked.
Which results in any write to these registers are ignored.

So, move the CTRL_MMR unlock to a point after the TIFS version message is
received, ensuring that the firmware initialization has progressed and
CTRL_MMR can be unlocked successfully.

Link: https://lore.kernel.org/all/20260225051559.416445-1-u-kumar1@ti.com/
Fixes: 5e291ee34fd4 ("arch: mach-k3: Introduce basic files to support J722S SoC family")
Reported-by: Diwakar Dhyani <d-dhyani@ti.com>
Signed-off-by: Udit Kumar <u-kumar1@ti.com>

arch/arm/mach-k3/j722s/j722s_init.c
@@ -84,8 +84,6 @@ static void k3_spl_init(void)
 	 */
 	store_boot_info_from_rom();
 
-	ctrl_mmr_unlock();
-
 	/* Init DM early */
 	ret = spl_early_init();
 	if (ret)
@@ -150,6 +148,8 @@ static void k3_spl_init(void)
 
 	/* Output System Firmware version info */
 	k3_sysfw_print_ver();
+
+	ctrl_mmr_unlock();
 
 	/* Output DM Firmware version info */
 	if (IS_ENABLED(CONFIG_ARM64))

thom24/u-boot

commit 5972e4dd2cc80faafa5cd60540293b2714ae9417

DONOTMERGE: TI: mach-k3: reapply CTRL_MMR access before spl_early_init()
Since commit f68a776b69d3 ("FROMLIST: mach-k3: Defer CTRL_MMR access until
after TIFS version response"), TFA fails to start on j722s.

Reapplying ctrl_mmr_unlock() before spl_early_init() fixes it.

Fixes: f68a776b69d3 ("FROMLIST: mach-k3: Defer CTRL_MMR access until after TIFS version response")
Reviewed-by: Udit Kumar <u-kumar1@ti.com>
Signed-off-by: Richard Genoud (TI) <richard.genoud@bootlin.com>

arch/arm/mach-k3/j722s/j722s_init.c
@@ -87,6 +87,8 @@ static void k3_spl_init(void)
 	 */
 	store_boot_info_from_rom();
 
+	ctrl_mmr_unlock();
+
 	/* Init DM early */
 	ret = spl_early_init();
 	if (ret)